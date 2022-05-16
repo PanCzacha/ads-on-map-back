034337c64f9fe1484648e3ed6d80e784f9a51531
Krzysztof C <krzysztof.c.krzysztof@gmail.com>
2022-05-16 15:13:42 +0200

    Added global error handling.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, {json} from "express";
 import cors from "cors";
+import { handleError } from "./utils/error";
 import "express-async-errors";
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(cors({
     origin: "http://localhost:3000",
 }));
 app.use(json());
+
+app.use(handleError);
 
 app.listen(3001, "0.0.0.0", () => {
     console.log("Listening on port http://localhost:3001")
