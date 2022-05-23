d4698600a5ddec4d4e1ac0d33e55f45702a733b5
Krzysztof C <krzysztof.c.krzysztof@gmail.com>
2022-05-23 13:23:37 +0200

    Added rate limiter

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, {json, NextFunction, Request, Response} from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit"
 import {handleError} from "./utils/error";
 import "express-async-errors";
 import {AdRecord} from "./records/ad.record";
@@ -7,11 +8,19 @@ import {MarkupsList} from "./types";
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+
 app.use(cors({
     origin: "http://localhost:3000",
 }));
 app.use(json());
-
+app.use(limiter);
 app.use(handleError);
 
 app.get("/list", async (req: Request, res: Response, next: NextFunction) => {
